df55c37e867e482fd4d6f413231a6a8763799ded
ArinaKumeisha <arinanozdracheva@mail.ru>
2021-07-02 13:36:34 +0300

    homework_3 scripts

--- src/p2-homeworks/h3/Greeting.tsx
+++ src/p2-homeworks/h3/Greeting.tsx
@@ -1,12 +1,12 @@
-import React, {ChangeEvent,KeyboardEvent} from 'react'
+import React, {ChangeEvent, KeyboardEvent} from 'react'
 import s from './Greeting.module.css'
 import {UserType} from "./HW3";
 
 type GreetingPropsType = {
     name: string
-    setNameCallback: (e: ChangeEvent<HTMLInputElement>)=>void
-    addUser: ()=> void
-    error: string
+    setNameCallback: (e: ChangeEvent<HTMLInputElement>) => void
+    addUser: () => void
+    error: boolean
     totalUsers: number
     onEnter: (e: KeyboardEvent<HTMLInputElement>) => void
 
@@ -16,8 +16,8 @@ type GreetingPropsType = {
 const Greeting: React.FC<GreetingPropsType> = (
     {name, setNameCallback, addUser, error, totalUsers, onEnter} // деструктуризация пропсов
 ) => {
-    const inputClass = error? s.errorInput: s.input
-
+    const inputClass = error ? s.errorInput : s.input
+    const errorMessage = error ? <div style={{color: "red"}}>Name is required!!!</div> : null
     return (
         <div className={s.greeting}>
             <input value={name}
@@ -25,10 +25,8 @@ const Greeting: React.FC<GreetingPropsType> = (
                    className={inputClass}
                    onKeyDown={onEnter}
             />
-            <div className={s.error}>{error}</div>
-            <span>{error}</span>
+            <span>{errorMessage}</span>
             <button onClick={addUser} className={s.button} disabled={!name}>add</button>
-            <div className={s.count}>{totalUsers}</div>
             <span>{totalUsers}</span>
         </div>
     )
--- src/p2-homeworks/h3/GreetingContainer.tsx
+++ src/p2-homeworks/h3/GreetingContainer.tsx
@@ -4,28 +4,31 @@ import {UserType} from "./HW3";
 
 type GreetingContainerPropsType = {
     users: Array<UserType>
-    addUserCallback: (name:string)=> void
+    addUserCallback: (name: string) => void
 
 }
 
 
 // уровень локальной логики
-const GreetingContainer: React.FC<GreetingContainerPropsType> = ({users,addUserCallback}) => { // деструктуризация пропсов
+const GreetingContainer: React.FC<GreetingContainerPropsType> = (
+    {users, addUserCallback}
+) => { // деструктуризация пропсов
     const [name, setName] = useState<string>("")
-    const [error, setError] = useState<string>("")
+    const [error, setError] = useState<boolean>(false)
 
     const setNameCallback = (e: ChangeEvent<HTMLInputElement>) => {
-      const trimmedName=e.currentTarget.value.trim()
-        if(trimmedName){
+        const trimmedName = e.currentTarget.value.trim()
+        if (trimmedName) {
             setName(trimmedName)
-        }else{
+        } else {
+            setError(true)
             setName("")
-            setError("name is require")
+
         }
     }
     const addUser = () => {
         addUserCallback(name)
-    alert(`hello ${name}`)
+        alert(`hello ${name}`)
         setName("")
     }
 
